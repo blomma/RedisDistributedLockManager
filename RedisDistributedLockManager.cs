2f1b62aac9fa7bb2ef8415ad0c4cd53cd02adb30
Mikael Hultgren <blomma@artsoftheinsane.com>
2020-04-20 19:44:37 +0200

    Initial commit

---
+++ RedisDistributedLockManager.cs
@@ -0,0 +1,122 @@
+namespace Infrastructure {
+    using System;
+    using System.Threading.Tasks;
+    using StackExchange.Redis;
+
+    public class RedisDistributedLock {
+        public RedisKey Key { get; private set; }
+
+        public RedisValue Value { get; private set; }
+
+        public TimeSpan Validity { get; private set; }
+
+        public RedisDistributedLock(RedisKey key, RedisValue value, TimeSpan validity) {
+            Key = key;
+            Value = value;
+            Validity = validity;
+        }
+    }
+
+    public class RedisDistributedLockManager : IDisposable {
+        private readonly IConnectionMultiplexer connectionMultiplexer;
+        private RedisDistributedLock redisDistributedLock;
+
+        public RedisDistributedLockManager(IConnectionMultiplexer connectionMultiplexer) {
+            this.connectionMultiplexer = connectionMultiplexer;
+        }
+
+        private bool IsConnected() {
+            if (connectionMultiplexer == null) {
+                return false;
+            }
+
+            return connectionMultiplexer.IsConnected;
+        }
+
+        private const string UnlockScript = @"
+            if redis.call(""get"",KEYS[1]) == ARGV[1] then
+                return redis.call(""del"",KEYS[1])
+            else
+                return 0
+            end";
+
+        private byte[] CreateUniqueLockId() {
+            return Guid.NewGuid().ToByteArray();
+        }
+
+        public async Task<bool> LockAsync(string key, TimeSpan ttl) {
+            if (!IsConnected()) {
+                return false;
+            }
+
+            var value = CreateUniqueLockId();
+            var result = await connectionMultiplexer.GetDatabase().StringSetAsync(key, value, ttl, When.NotExists);
+            if (!result) {
+                return false;
+            }
+
+            redisDistributedLock = new RedisDistributedLock(key, value, ttl);
+
+            return true;
+        }
+
+        public bool Lock(string key, TimeSpan ttl) {
+            if (!IsConnected()) {
+                return false;
+            }
+
+            var value = CreateUniqueLockId();
+            var result = connectionMultiplexer.GetDatabase().StringSet(key, value, ttl, When.NotExists);
+            if (!result) {
+                return false;
+            }
+
+            redisDistributedLock = new RedisDistributedLock(key, value, ttl);
+
+            return true;
+        }
+
+        public Task UnlockAsync() {
+            RedisKey[] key = { redisDistributedLock.Key };
+            RedisValue[] values = { redisDistributedLock.Value };
+
+            return connectionMultiplexer.GetDatabase().ScriptEvaluateAsync(
+                UnlockScript,
+                key,
+                values
+            );
+        }
+
+        public void Unlock() {
+            if (redisDistributedLock == null) {
+                return;
+            }
+
+            RedisKey[] key = { redisDistributedLock.Key };
+            RedisValue[] values = { redisDistributedLock.Value };
+            connectionMultiplexer.GetDatabase().ScriptEvaluate(
+                UnlockScript,
+                key,
+                values
+            );
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing) {
+            if (!disposedValue) {
+                if (disposing) {
+                    Unlock();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose() {
+            Dispose(true);
+        }
+        #endregion
+    }
+}
